89977715a5f2593f9a560b65c54a9c51c5409f8e
abdi-edoc-de <61737042+abdi-edoc-de@users.noreply.github.com>
2022-02-20 10:17:54 +0300

    Time: 54 ms (48.92%), Space: 14 MB (86.49%) - LeetHub

--- 833-find-and-replace-in-string/833-find-and-replace-in-string.py
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.py
@@ -1,27 +1,12 @@
 class Solution:
     def findReplaceString(self, s: str, indices: List[int], sources: List[str], targets: List[str]) -> str:
-        n = len(indices)
-        all = []
-        for i in range(n):
-            all.append([indices[i] , sources[i], targets[i]])
-        def is_substring(sub,ind):
-            nsub,ns = len(sub), len(s)
-            # print(ind, sub,s)
-            for i in range(nsub):
-                if ind+i > ns or s[ind+i] != sub[i]:return False
-            return True
-        offset = 0
-        all.sort()
-        for i in  range(n):
-            ind, sub , target = all[i]
-            ind += offset
-            if is_substring(sub, ind):
-                s = s[:ind] + target + s[ind+len(sub):]
-                offset += len(target) - len(sub)
-                print(ind, sub ,s)
-            
-        return s
-            
-            
-        
-        
+        result = list(s)
+        for ind, target, source in zip(indices, targets, sources):
+            if not s.startswith(source, ind):
+                continue
+            else:
+                result[ind] = target
+                for i in range(ind+1, len(source)+ind):
+                    result[i] = ''
+        return ''.join(result)
+    
