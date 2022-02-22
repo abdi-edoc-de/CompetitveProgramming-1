d0084986209c1923db41852d3970724072e76b45
abdi-edoc-de <61737042+abdi-edoc-de@users.noreply.github.com>
2022-02-22 23:07:05 +0300

    Time: 82 ms (27.73%), Space: 13.9 MB (77.92%) - LeetHub

--- 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.py
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.py
@@ -10,10 +10,6 @@ class Solution:
             no_odds_col += int(cols[i] % 2 != 0)
             no_even_col -= int( not cols[i] % 2 == 0)
         for i in range(m):
-            # print(rows[i])
             no_odds_row += int(rows[i] % 2 != 0)
             no_even_row -= int(not rows[i] % 2 == 0)
-        # print(rows, cols)
-        # print(int(1 % 2 == 0))
-        # print(no_odds_row, no_even_col , no_odds_col, no_even_row)
         return (no_odds_row * no_even_col) + (no_odds_col * no_even_row)
