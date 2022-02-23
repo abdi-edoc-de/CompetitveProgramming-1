5f70b8b00d16c532545ced100a56a816b154113b
abdi-edoc-de <61737042+abdi-edoc-de@users.noreply.github.com>
2022-02-23 14:30:25 +0300

    Time: 57 ms (43.02%), Space: 14.3 MB (91.72%) - LeetHub

--- 133-clone-graph/133-clone-graph.py
+++ 133-clone-graph/133-clone-graph.py
@@ -1,21 +1,25 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
-        if not node:return 
-        visited = set()
-        graph = {}
-        self.dfs(node,visited , graph)
-        # self.dfss(graph[node.val],set())
-        return graph[node.val]
-    def dfs(self, node,visited, graph):
-        if node.val not in graph:
-            graph[node.val] = Node(node.val)
-        for neighbour in node.neighbors:
-            if (node.val, neighbour.val) in visited or ( neighbour.val, node.val) in visited :
-                continue
-            if neighbour.val not in graph:
-                graph[neighbour.val] = Node(neighbour.val)
-            visited.add((node.val, neighbour.val))
-            visited.add(( neighbour.val,node.val))
-            self.dfs(neighbour, visited, graph)
-            graph[node.val].neighbors.append(graph[neighbour.val])
-            graph[neighbour.val].neighbors.append(graph[node.val])    
+        nodes = {}
+        # print(node.val, 2, node.neighbors[0].val)
+        visited = {}
+        def copy(node):
+            if not node:return node
+            if  node.val not in nodes:
+                nodes[node.val] = Node(node.val)
+            new_node = nodes[node.val]
+            for nei in node.neighbors:
+                if nei.val in nodes:
+                    new_node.neighbors.append(nodes[nei.val])
+                else:
+                    new_node.neighbors.append(copy(nei))
+            return nodes[node.val]
+        return copy(node)
