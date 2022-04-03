af6fa761d903604ac020ee6367207e0762633efe
abdi-edoc-de <61737042+abdi-edoc-de@users.noreply.github.com>
2022-04-03 06:42:27 +0300

    Time: 52 ms (45.62%), Space: 13.9 MB (11.40%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,10 +1,7 @@
 class Solution:
     def fib(self, n: int) -> int:
-        a, b =0 , 1
-        if n ==0:return 0
-        for i in range(2,n+1):
-            b, a = a + b,b
-        return b
-            
-                
-        
+        if n== 0:return 0
+        left, right = 0, 1
+        for num in range(2,n+1):
+            left , right = right , left + right
+        return right 
