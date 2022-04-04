ffc03e205566973cc32f10ef1a0e45026218ac7c
abdi-edoc-de <61737042+abdi-edoc-de@users.noreply.github.com>
2022-04-04 09:17:32 +0300

    Time: 99 ms (29.71%), Space: 16.9 MB (5.17%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,15 +1,13 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        
         dp = {}
-        
-        n = len(cost) - 1
-        def recur(ind):
-            if ind >n :return 0
-            if ind in dp:return dp[ind]
-            dp[ind] = min(recur(ind+1), recur(ind+2)) + cost[ind]
+        n =len(cost)
+        @cache
+        def dfs(ind):
+            if ind >= n :
+                return 0
+            dp[ind] = min(dfs(ind+1), dfs(ind+2)) + cost[ind]
             return dp[ind]
-        zero = recur(0)
-        dp = {}
-        one = recur(1)
-        return min(zero, one)
+    
+        return min(dfs(0),dfs(1))
+    
